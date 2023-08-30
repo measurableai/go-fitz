84612132fe4c0b7c3f87389a88faa85d2994b46e
Matt Lung <matt@mailtime.com>
2023-08-30 11:15:40 +0800

    Update import paths

--- example_test.go
+++ example_test.go
@@ -6,7 +6,7 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/gen2brain/go-fitz"
+	"github.com/measurableai/go-fitz"
 )
 
 func ExampleNew() {
--- fitz_test.go
+++ fitz_test.go
@@ -9,7 +9,7 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/gen2brain/go-fitz"
+	"github.com/measurableai/go-fitz"
 )
 
 func TestImage(t *testing.T) {
